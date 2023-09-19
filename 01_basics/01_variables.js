18ac2c18338207f24440cafa1e25010da5470f08
lokeshshriwas <99093170+lokeshshriwas@users.noreply.github.com>
2023-09-19 18:35:27 +0000

    added conversion_operation.js file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 2802;
+let accountEmail = "lokesh2802@gmail.com";
+var accountPassward = "12345";
+accountCity = 'kawardha'
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "lokeshshriwas2802@google.com"
+accountPassward = "54321"
+accountCity = "Raipur"
+
+console.table([accountEmail, accountId, accountPassward, accountState, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+//  alert(3+3) // we are using node js not browser
+
+console.log("lokesh") // code must be readable
+
+let name = "lokesh";
+let age = 21;
+let isLoggedIn = False;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "lokesh")
