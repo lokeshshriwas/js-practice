80ced5228c87801f77979c6c89685e85cca59e98
lokeshshriwas <99093170+lokeshshriwas@users.noreply.github.com>
2023-09-27 02:58:15 +0530

    added filter, chaining, and reduce method files

---
+++ 05_iterators/07_chaining.js
@@ -0,0 +1,12 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+// this method is called chaining which helps us to creat several conditions in very simple way and easily
+
+console.log(newNums);
